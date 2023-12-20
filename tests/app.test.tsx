bb5bd580192c3412969e6331497283603819ae78
Caisa Kohlin <caisa.kohlin@gmail.com>
2023-12-20 12:08:23 +0100

    comment and format

--- tests/app.test.tsx
+++ tests/app.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen, } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../src/App';
 
-
+//This is an example test to see that the testing works
+//Remove this test when you start writing your own tests
 test('should render hello', () => {
-    render(<App />);
-    const h1 = screen.getByRole('heading', { name: 'hello' });
-    expect(h1).toBeInTheDocument();
-  });
+  render(<App />);
+  const h1 = screen.getByRole('heading', { name: 'hello' });
+  expect(h1).toBeInTheDocument();
+});
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config';
 
-    export default defineConfig({
-        test: {
-          globals: true,
-          environment: 'jsdom',
-          setupFiles: '/tests/setup.ts',
-        },
-      })
+//TODO make the vite config and vitest config into the same file
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: '/tests/setup.ts',
+  },
+});
