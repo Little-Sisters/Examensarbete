d68ece686d45b44bffad7fc97368c0929dae3398
felicia <feliciawillnas@gmail.com>
2024-01-02 12:32:16 +0100

    footer set up + react icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,31 @@
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <>
+      <footer>
+        <div>
+          <h1>BEAUTIFUL CAKES LOGO</h1>
+        </div>
+        <div>
+          <h1>CONTACT US</h1>
+          <FaTwitter />
+          <FaYoutube />
+          <FaInstagram />
+          <FaFacebook />
+        </div>
+        <div>
+          <h1>NEWSLETTER</h1>
+          <input type="text" />
+          <input type="button" value="" />
+        </div>
+        <div>
+          <h1>GET IN TOUCH</h1>
+          <p>Number</p>
+          <p>Email</p>
+        </div>
+      </footer>
+    </>
+  );
 }
 
 export default Footer;
