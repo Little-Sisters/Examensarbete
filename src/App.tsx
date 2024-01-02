cec34af59960ac39ddb3931c8c8643c9651062a6
Caisa Kohlin <caisa.kohlin@gmail.com>
2024-01-02 12:01:45 +0100

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -28,97 +28,100 @@ export function App() {
 
   return (
     <ThemeProvider theme={theme === 'light' ? lightMode : darkMode}>
-    <>
-      <GlobalStyles />
-      <Header />
-      <main style={{position:'relative'}}>
-        <AnimatePresence initial={false} mode='wait'>
-          <Routes location={location} key={locationArr[1]}>
-            <Route
-              path="/"
-              element={
-                <Page>
-                  <HomePage />
-                </Page>
-              }
-            />
-            <Route
-              path="/flavors/*"
-              element={
-                <Page>
-                  <FlavorsPage />
-                  {/** Vi behöver använda absolute positionering om vi vill att content ska fadea in samtidigt som den andra fadear ut, div
-                   * här är höjden på "outlet"*/}
-                  <div style={{ position: 'relative', height: 200 }}>
-                    <AnimatePresence initial={false}>
-                      <Routes location={location} key={locationArr[2]}>
-                        <Route
-                          path="/*"
-                          element={
-                            <NestedPage
-                              title="chocolate"
-                              nextPath="../vanilla"
+      <>
+        <GlobalStyles />
+        <StyleSheetManager shouldForwardProp={shouldForwardProp}>
+          <Header />
+          <button onClick={themeToggler}>Switch Theme</button>
+          <main style={{ position: 'relative' }}>
+            <AnimatePresence initial={false} mode="wait">
+              <Routes location={location} key={locationArr[1]}>
+                <Route
+                  path="/"
+                  element={
+                    <Page>
+                      <HomePage />
+                    </Page>
+                  }
+                />
+                <Route
+                  path="/flavors/*"
+                  element={
+                    <Page>
+                      <FlavorsPage />
+                      {/** Vi behöver använda absolute positionering om vi vill att content ska fadea in samtidigt som den andra fadear ut, div
+                       * här är höjden på "outlet"*/}
+                      <div style={{ position: 'relative', height: 200 }}>
+                        <AnimatePresence initial={false}>
+                          <Routes location={location} key={locationArr[2]}>
+                            <Route
+                              path="/*"
+                              element={
+                                <NestedPage
+                                  title="chocolate"
+                                  nextPath="../vanilla"
+                                />
+                              }
                             />
-                          }
-                        />
-                        <Route
-                          path="/vanilla"
-                          element={
-                            <NestedPage
-                              title="Vanilla"
-                              nextPath="../red-velvet"
+                            <Route
+                              path="/vanilla"
+                              element={
+                                <NestedPage
+                                  title="Vanilla"
+                                  nextPath="../red-velvet"
+                                />
+                              }
                             />
-                          }
-                        />
-                        <Route
-                          path="/red-velvet"
-                          element={
-                            <NestedPage title="Red Velvet" nextPath="../" />
-                          }
-                        />
-                      </Routes>
-                    </AnimatePresence>
-                  </div>
-                </Page>
-              }
-            />
-            <Route
-              path="/gallery/*"
-              element={
-                <Page>
-                  <GalleryPage />
-                </Page>
-              }
-            />
-            <Route
-              path="/about/*"
-              element={
-                <Page>
-                  <AboutUsPage />
-                </Page>
-              }
-            />
-            <Route
-              path="/products/*"
-              element={
-                <Page>
-                  <ProductPage />
-                </Page>
-              }
-            />
-            <Route
-              path="/cart/*"
-              element={
-                <Page>
-                  <CartPage />
-                </Page>
-              }
-            />
-          </Routes>
-        </AnimatePresence>
-      </main>
-      <Footer />
-    </>
+                            <Route
+                              path="/red-velvet"
+                              element={
+                                <NestedPage title="Red Velvet" nextPath="../" />
+                              }
+                            />
+                          </Routes>
+                        </AnimatePresence>
+                      </div>
+                    </Page>
+                  }
+                />
+                <Route
+                  path="/gallery/*"
+                  element={
+                    <Page>
+                      <GalleryPage />
+                    </Page>
+                  }
+                />
+                <Route
+                  path="/about/*"
+                  element={
+                    <Page>
+                      <AboutUsPage />
+                    </Page>
+                  }
+                />
+                <Route
+                  path="/products/*"
+                  element={
+                    <Page>
+                      <ProductPage />
+                    </Page>
+                  }
+                />
+                <Route
+                  path="/cart/*"
+                  element={
+                    <Page>
+                      <CartPage />
+                    </Page>
+                  }
+                />
+              </Routes>
+            </AnimatePresence>
+          </main>
+          <Footer />
+        </StyleSheetManager>
+      </>
     </ThemeProvider>
   );
 }
