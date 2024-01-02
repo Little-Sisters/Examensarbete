4659dd6f387adc35b28e28b3e02eba9c05d8ec6c
felicia <feliciawillnas@gmail.com>
2024-01-02 14:35:10 +0100

    small style update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,28 +1,37 @@
 import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
 function Footer() {
   return (
     <>
       <footer>
-        <div>
+        <StyledFooter>
           <h1>BEAUTIFUL CAKES LOGO</h1>
-        </div>
-        <div>
-          <h1>CONTACT US</h1>
-          <FaTwitter />
-          <FaYoutube />
-          <FaInstagram />
-          <FaFacebook />
-        </div>
-        <div>
-          <h1>NEWSLETTER</h1>
-          <input type="text" />
-          <input type="button" value="" />
-        </div>
-        <div>
-          <h1>GET IN TOUCH</h1>
-          <p>Number</p>
-          <p>Email</p>
-        </div>
+
+          <div>
+            <h1>CONTACT US</h1>
+            <FaTwitter />
+            <FaYoutube />
+            <FaInstagram />
+            <FaFacebook />
+          </div>
+          <div>
+            <h1>NEWSLETTER</h1>
+            <input type="text" />
+            <input type="button" value="" />
+          </div>
+          <div>
+            <h1>GET IN TOUCH</h1>
+            <p>Number</p>
+            <p>Email</p>
+          </div>
+        </StyledFooter>
       </footer>
     </>
   );
