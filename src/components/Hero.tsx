a4bfa57f2a14ae0dbfa2d2e917a3b375a9d893b4
Caisa Kohlin <caisa.kohlin@gmail.com>
2024-01-02 13:04:25 +0100

    hero

---
+++ src/components/Hero.tsx
@@ -0,0 +1,67 @@
+import herocake from '/herocake.jpg';
+import styled from 'styled-components';
+
+function Hero() {
+  return (
+    <>
+      <HeroContainer>
+        <StyledImage src={herocake} alt="cake" />
+        <HeroText>Beautiful Cakes</HeroText>
+        <BottomRightContainer>
+          <SubText>Customize your cake</SubText>
+          <Button href="/products">Go to Products</Button>
+        </BottomRightContainer>
+      </HeroContainer>
+    </>
+  );
+}
+
+const StyledImage = styled.img`
+  object-fit: cover;
+  width: 100%;
+  height: 80vh;
+`;
+
+const HeroContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 80vh;
+`;
+
+const HeroText = styled.h1`
+  position: absolute;
+  top: 10%;
+  left: 50%;
+  transform: translate(-50%, -10%);
+  color: white;
+  font-size: 3rem;
+  text-align: center;
+`;
+
+const BottomRightContainer = styled.div`
+  position: absolute;
+  bottom: 10%;
+  right: 10%;
+  text-align: right;
+  color: white;
+`;
+
+const SubText = styled.h2`
+  font-size: 2rem;
+  margin-bottom: 1rem;
+`;
+
+const Button = styled.a`
+  display: inline-block;
+  color: white;
+  padding: 0.5rem 1rem;
+  text-decoration: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: bold;
+  transition: background-color 0.3s;
+  background: ${({ theme }) => theme.button};
+  color: ${({ theme }) => theme.text};
+`;
+
+export default Hero;
