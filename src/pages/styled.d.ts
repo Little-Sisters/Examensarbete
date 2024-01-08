a13d07fdc4b6b251d0797276715fe9bb47b25fad
mims <113519976+MimPMC@users.noreply.github.com>
2024-01-08 11:48:58 +0100

    headerlogo reusable

--- src/pages/styled.d.ts
+++ src/pages/styled.d.ts
@@ -1,15 +1,24 @@
+// styled.d.ts
+
 import 'styled-components';
 
 // Defines theme interface and extends DefaultTheme
-// This is used so that typescript knows what the theme object looks like and doesnt throw errors
+// This is used so that typescript knows what the theme object looks like and doesn't throw errors
 declare module 'styled-components' {
   export interface DefaultTheme {
+    mode: string;
     body: string;
+    bodyOpacity: string;
     text: string;
+    textHover: string;
     input: string;
     card: string;
     background: string;
     button: string;
     paper: string;
+    footer: string;
+    footerDetail: string;
+    footerText: string;
+    black: string;
   }
 }
