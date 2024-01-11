f821d1b16c3bce530c9fd0e9efaeef5e17184dd2
Caisa Kohlin <caisa.kohlin@gmail.com>
2024-01-11 16:30:03 +0100

    confirmation page WIP

--- src/components/Envelope.tsx
+++ src/components/Envelope.tsx
@@ -1,59 +1,89 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { motion, useTransform, useScroll } from 'framer-motion';
+import { useOrder } from '../contexts/OrderContext';
 
 interface EnvelopeProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 
 const Envelope: React.FC<EnvelopeProps> = ({ children }) => {
-    const [ffLayer, setFfLayer] = useState<number>(0);
-    const { scrollYProgress } = useScroll();
-    const scaleAnim = useTransform(scrollYProgress, [0, 0.5, 1], [1, 1.2, 0.8]);
-    const yPosAnim = useTransform(scrollYProgress, [0, 0.5, 1], [0, 100, 200]);
-    const zRotAnim = useTransform(scrollYProgress, [0, 0.5, 1], [0, 3, 0]);
+  const [ffLayer, setFfLayer] = useState<number>(0);
+  const { scrollYProgress } = useScroll();
+  const scaleAnim = useTransform(scrollYProgress, [0, 0.5, 1], [1, 1.2, 0.8]);
+  const yPosAnim = useTransform(scrollYProgress, [0, 0.5, 1], [0, 100, 200]);
+  const zRotAnim = useTransform(scrollYProgress, [0, 0.5, 1], [0, 3, 0]);
+  const { getLastOrder } = useOrder();
+  const { lastOrder } = getLastOrder();
 
-    scrollYProgress.onChange(x => {
-        setFfLayer(x > 0.4 ? -1 : 0);
-    });
+  scrollYProgress.onChange((x) => {
+    setFfLayer(x > 0.4 ? -1 : 0);
+  });
 
-    return (
-        <StyledEnvelope
-            style={{
-                scale: scaleAnim,
-                y: yPosAnim,
-                rotateZ: zRotAnim,
-            }}
-        >
-            {children}
-            <Frontface style={{ zIndex: ffLayer }}>
-                <button onClick={() => window.scrollTo(0, 1500)}>Open Me</button>
-            </Frontface>
-        </StyledEnvelope>
-    );
+  return (
+    <StyledEnvelope
+      style={{
+        scale: scaleAnim,
+        y: yPosAnim,
+        rotateZ: zRotAnim,
+      }}
+    >
+      {children}
+      <Frontface style={{ zIndex: ffLayer }}>
+        <FlexContainer>
+          <h2>To</h2>
+          <div>
+            {lastOrder ? (
+              <div>
+                <StyledText>{lastOrder?.contactInformation.name}</StyledText>
+                <StyledText>{lastOrder?.contactInformation.street}</StyledText>
+                <StyledText>{lastOrder?.contactInformation.zipCode}</StyledText>
+                <StyledText>{lastOrder?.contactInformation.city}</StyledText>
+                <StyledText>{lastOrder?.contactInformation.email}</StyledText>
+                <StyledText>{lastOrder?.contactInformation.phone}</StyledText>
+              </div>
+            ) : (
+              <p>No stored values found.</p>
+            )}
+          </div>
+          <button onClick={() => window.scrollTo(0, 1500)}>Open Me</button>
+        </FlexContainer>
+      </Frontface>
+    </StyledEnvelope>
+  );
 };
 
 // Styled components
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const StyledText = styled.p`
+  font-size: 12px;
+`;
+
 const StyledEnvelope = styled(motion.div)`
-    width: 28rem;
-    height: 15rem;
-    scale: 1;
-    position: fixed;
-    top: 10rem;
-    left: calc(50% - 14rem);
-    box-shadow: rgba(0, 0, 0, 0.5) 0px 0px 150px 10px;
+  width: 28rem;
+  height: 20rem;
+  scale: 1;
+  position: fixed;
+  top: 10rem;
+  left: calc(50% - 14rem);
+  box-shadow: rgba(0, 0, 0, 0.5) 0px 0px 150px 10px;
 `;
 
 const Frontface = styled.div`
-width: 100%;
-height: 100%;
-background-color: #debda1;
-position: absolute;
-left: 0;
-top: 0;
-display: flex;
-justify-content: center;
-align-items: center;
+  width: 100%;
+  height: 100%;
+  background-color: #debda1;
+  position: absolute;
+  left: 0;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default Envelope;
--- src/components/Letter.tsx
+++ src/components/Letter.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import styled from 'styled-components';
 import { motion, useTransform, useViewportScroll } from 'framer-motion';
+import { useOrder } from '../contexts/OrderContext';
+import { useCart } from '../contexts/CartContext';
+
+// Generates unique number
+// Checks if the number is already in use in Local Storage
+// If it is, generate a new number
+// If it isn't, store the number in Local Storage and return it
+export function generateUniqueNumber(): number {
+    const number = Math.floor(Math.random() * 90000) + 10000;
+    if (localStorage.getItem(number.toString())) {
+      return generateUniqueNumber();
+    } else {
+      localStorage.setItem(number.toString(), 'true');
+      return number;
+    }
+  }
 
 const Letter: React.FC = () => {
     const { scrollYProgress } = useViewportScroll();
     const scaleAnim = useTransform(scrollYProgress, [0, 0.5, 1], [1, 1, 1.5]);
     const yPosAnim = useTransform(scrollYProgress, [0, 0.4, 1], [0, -250, -100]);
+
+    const { getLastOrder } = useOrder();
+    const { cartList } = useCart();
+    const { lastOrder } = getLastOrder();
+
+    const totalPrice = cartList.reduce((total, cartItem) => {
+        return total + cartItem.quantity * cartItem.price;
+      }, 0);
+      console.log(totalPrice);
+    
 
     return (
         <StyledLetter
@@ -14,14 +40,36 @@ const Letter: React.FC = () => {
                 y: yPosAnim,
             }}
         >
-            <h1>this is the letter</h1>
-            <p>this is product info</p>
-            <p>i want fast food</p>
+            <StyledText>Thank you {lastOrder?.contactInformation.name} for your order! Your
+            order id is: #{lastOrder?.orderId}</StyledText>
+            <StyledUnorderedList>
+            {lastOrder?.itemList.map((cartItem) => (
+              <li key={cartItem.id}>
+                <div>
+                  <div>
+                    <StyledImage src={cartItem.image} alt={cartItem.imageAlt} />
+                  </div>
+                  <p>{cartItem.title}</p>
+                  <p>${cartItem.quantity * cartItem.price}</p>
+                  <p>Total: ${lastOrder?.totalPrice}</p>
+                </div>
+              </li>
+            ))}
+            </StyledUnorderedList>
+            <StyledFlexCenter>
+            <StyledLink href="/">
+              <StyledButton type="button">Place another order</StyledButton>
+            </StyledLink>
+          </StyledFlexCenter>
         </StyledLetter>
     );
 };
 
-// Styled component
+// Styled components
+const StyledText = styled.p`
+ font-size: 12px;
+`;
+
 const StyledLetter = styled(motion.div)`
    width: calc(100% - 1rem);
    height: calc(100% - 1rem);
@@ -32,6 +80,41 @@ const StyledLetter = styled(motion.div)`
    padding: 1rem;
    box-sizing: border-box;
    position: absolute;
+`;
+
+const StyledImage = styled.img`
+height: 5rem;
+width: 3rem;
+`
+
+const StyledUnorderedList = styled.ul`
+  list-style-type: none;
+  margin-inline-start: 0;
+  font-size: 12px;
+`;
+
+const StyledFlexCenter = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledLink = styled.a`
+  margin: auto;
+  text-decoration: none;
+  &:hover {
+    text-decoration: none;
+  }
+`;
+
+const StyledButton = styled.button`
+  width: 8rem;
+  height: 2rem;
+  border: none;
+  font-size: 12px;
+  cursor: pointer;
+  &:hover {
+  }
 `;
 
 export default Letter;
