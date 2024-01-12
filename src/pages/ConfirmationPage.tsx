d051329d0a16fb67c0b8eca5f1da99d3c8e4ebe8
Caisa Kohlin <caisa.kohlin@gmail.com>
2024-01-12 17:20:50 +0100

    mobile confirmation page

--- src/pages/ConfirmationPage.tsx
+++ src/pages/ConfirmationPage.tsx
@@ -2,17 +2,31 @@ import styled from 'styled-components';
 import { useOrder } from '../contexts/OrderContext';
 import Letter from '../components/Letter';
 import Envelope from '../components/Envelope';
+import MobileConfirmation from '../components/MobileConfirmation';
+import MarginTopContainer from '../components/MarginTopContainer';
+import PageContentWrapper from '../components/PageContentWrapper';
+import useMobile from '../hooks/UseMobile';
 
 function ConfirmationPage() {
   const { getLastOrder } = useOrder();
   const { lastOrder } = getLastOrder();
+  const isMobile = useMobile(700);
   console.log('Last order:', lastOrder);
+
   return (
-    <StyledContainer>
-      <Envelope>
-        <Letter />
-      </Envelope>
-    </StyledContainer>
+    <MarginTopContainer>
+      <PageContentWrapper>
+      {isMobile ? (
+          <MobileConfirmation />
+        ) : (
+          <StyledContainer>
+            <Envelope>
+              <Letter />
+            </Envelope>
+          </StyledContainer>
+        )}
+      </PageContentWrapper>
+    </MarginTopContainer>
   );
 }
 
