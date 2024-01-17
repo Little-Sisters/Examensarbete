e2a15ccad5f68859a37a49a0c51c2facc676fa09
Caisa Kohlin <caisa.kohlin@gmail.com>
2024-01-17 11:39:07 +0100

    gallery page bento grid tests

--- tests/GalleryPage.test.tsx
+++ tests/GalleryPage.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import GalleryPage from '../src/pages/GalleryPage';
+import BentoGrid from '../src/components/BentoGrid';
+import { CellData, data } from '../data/cellData';
 
 describe('GalleryPage', () => {
   test('renders GalleryPage component', () => {
@@ -8,5 +10,28 @@ describe('GalleryPage', () => {
       name: 'Gallery',
     });
     expect(headingElement).toBeInTheDocument();
+  });
+});
+
+describe('BentoGrid Component', () => {
+  it('renders component', () => {
+    render(<BentoGrid />);
+    expect(screen.getByTestId('bento-grid')).toBeInTheDocument();
+  });
+
+  it('renders the correct number of cells', () => {
+    const { container } = render(<BentoGrid />);
+    data.forEach((item) => {
+      const cell = container.querySelector(`[data-id='${item.id}']`);
+      expect(cell).toBeInTheDocument();
+    });
+  });
+
+  it('displays correct information in each cell', () => {
+    render(<BentoGrid />);
+    data.forEach((item: CellData) => {
+      const matchingElements = screen.getAllByText(item.title);
+      expect(matchingElements.length).toBeGreaterThan(0);
+    });
   });
 });
