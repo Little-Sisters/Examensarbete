a75bfe390b2a9fe388d5707beaf659470b4b462b
Caisa Kohlin <caisa.kohlin@gmail.com>
2024-01-18 13:45:42 +0100

    remove footer from app and add to pages

--- src/pages/GalleryPage.tsx
+++ src/pages/GalleryPage.tsx
@@ -3,22 +3,29 @@ import MarginTopContainer from '../components/reusable components/MarginTopConta
 import PageContentWrapper from '../components/reusable components/PageContentWrapper';
 import PageDescription from '../components/reusable components/PageDescription';
 import { useEffect } from 'react';
+import { useScrollToTop } from '../hooks/useScrollToTop';
+import Footer from '../components/Footer';
 
 function GalleryPage() {
   // Sets the page title
   useEffect(() => {
     document.title = 'Gallery';
   }, []);
+
+  useScrollToTop();
   return (
-    <MarginTopContainer>
-      <PageContentWrapper>
-        <PageDescription
-          title="Gallery"
-          description="Welcome to our gallery, where you'll find a showcase of both customized and bespoke cakes, accompanied by a curated selection of choices for each cake. Find your dream cake or draw inspiration to create your own!"
-        />
-        <BentoGrid />
-      </PageContentWrapper>
-    </MarginTopContainer>
+    <div>
+      <MarginTopContainer>
+        <PageContentWrapper>
+          <PageDescription
+            title="Gallery"
+            description="Welcome to our gallery, where you'll find a showcase of both customized and bespoke cakes, accompanied by a curated selection of choices for each cake. Find your dream cake or draw inspiration to create your own!"
+          />
+          <BentoGrid />
+        </PageContentWrapper>
+      </MarginTopContainer>
+      <Footer />
+    </div>
   );
 }
 
