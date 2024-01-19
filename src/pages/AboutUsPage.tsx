19b832abf6ddd06a6b795ef85aa0a19c9106bbe6
Caisa Kohlin <caisa.kohlin@gmail.com>
2024-01-19 16:00:09 +0100

    prep for rebase

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,30 +1,14 @@
 import MarginTopContainer from '../components/reusable components/MarginTopContainer';
 import PageContentWrapper from '../components/reusable components/PageContentWrapper';
-import FillerComponent from '../components/FillerComponent';
-import SocialMedia from '../components/SocialMedia';
-import styled from 'styled-components';
+
 
 function AboutUsPage() {
   return (
     <PageContentWrapper>
       <MarginTopContainer>
-        <Wrapper>
-          <FillerComponent />
-          <SocialMedia />
-        </Wrapper>
       </MarginTopContainer>
     </PageContentWrapper>
   );
 }
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 4rem;
-
-  @media (max-width: 700px) {
-    gap: 1rem;
-  }
-`;
 
 export default AboutUsPage;
