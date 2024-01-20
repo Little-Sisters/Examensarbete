fc39e6f9a5e2a54c8566e225f35441c28a49f45e
Caisa Kohlin <caisa.kohlin@gmail.com>
2024-01-20 11:40:27 +0100

    mobile styling

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -139,8 +139,15 @@ const StyledText = styled.div`
     width: 100%;
     min-height: 40vh;
 
-    p {
+    p,
+    h2 {
       width: 100%;
+      text-align: center;
+    }
+
+    div:first-child {
+      align-items: center;
+      padding-left: 0;
     }
   }
 `;
